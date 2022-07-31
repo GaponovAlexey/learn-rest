ff873aa6a75d26e3ffc9821062de6ccc5782b2a4
kekelberi <alexei.gaponov@gmail.com>
2022-07-31 22:27:13 +0300

    save

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"path"
-	"runtime"
-
-	"github.com/sirupsen/logrus"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -16,19 +11,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", Home)
+	
 
-	loge := logrus.New()
-	loge.SetReportCaller(true)
-	loge.Formatter = &logrus.TextFormatter{
-		CallerPrettyfier: func(f *runtime.Frame) (function string, file string) {
-			filename := path.Base(f.File)
-			return fmt.Sprintf("%s()", f.Function), fmt.Sprintf("%s,%d", filename, f.Line)
-		},
-		DisableColors: true,
-		FullTimestamp: true,
-	}
-	os.MkdirAll("logs",0644)
 
-	loge.Infof("start")
 	http.ListenAndServe(":3000", nil)
 }
--- logging/logrus.go
+++ logging/logrus.go
@@ -2,12 +2,75 @@ package logging
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"runtime"
 
 	"github.com/sirupsen/logrus"
 )
 
+type writeHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (h *writeHook) Fire(e *logrus.Entry) error {
+	line, err := e.String()
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	for _, w := range h.Writer {
+		_, err := w.Write([]byte(line))
+	}
+	return err
+}
+
+func (h *writeHook) levels() []logrus.Level {
+	return h.LogLevels
+}
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() *Logger {
+	return &Logger{e}
+}
+
+func (l *Logger) lWithField(k string, v interface{}) *Logger {
+	return &Logger{l.WithField(k, v)}
+}
+func (l *Logger) lWithField(f map[string]interface{}) *Logger {
+	return &Logger{l.WithFields(f)}
+}
+
 func Init() {
+	logrusLevel, err := logrus.ParseLevel(level)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	l := logrus.New()
 	l.SetReportCaller(true)
-	
+	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			filename := path.Base(f.File)
+			return fmt.Sprintf("%v %v", filename, f.Line), fmt.Sprintf("%s", f.Function)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	}
+	l.SetOutput(ioutil.Discard)
+	l.AddHook(&writeHook{
+		Writer:    []io.Writer{os.Stdout},
+		LogLevels: logrus.AllLevels,
+	})
+
+	l.SetLevel(logrus.TraceLevel)
+	e = logrus.NewEntry(l)
 }
